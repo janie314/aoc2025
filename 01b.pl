9f4ef6f44819287b0989623c0ce303105b52c9ea
12ball <pub@janie.page>
2025-12-01 20:40:53 -0600

    1

---
+++ 01b.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+
+my $i = 50;
+my $next = 0;
+my $res = 0;
+
+while (<>) {
+    my ($lr, $num) = m/([LR])([0-9]+)/;
+    $num = int($num);
+    my $sgn = $lr eq 'L' ? -1 : 1;
+    if ($i != 0 and $sgn == -1 and $num >= $i) {
+        $res += int(($num - $i) / 100) + 1;
+        print "$i\t$num\n";
+    } elsif ($i != 0 and $sgn == 1 and $num >= 100 - $i) {
+        $res += int(($num + $i) / 100);
+        print "$i\t$num\n";
+    } elsif ($i == 0) {
+        $res += int($num / 100);
+        print "$i\t$num\n";
+    }
+    $i = ($i + $sgn * $num) % 100;
+}
+
+print $res . "\n";
