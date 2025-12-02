500c09b5c4aca954a3273f3dbe9ad1223621e568
j <jandavis@epic.com>
2025-12-02 12:41:31 -0600

    02

---
+++ 02.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use re 'strict';
+
+my $sum  = 0;
+my $sum2 = 0;
+
+while (<>)
+{
+    foreach my $range (split ',')
+    {
+        my ($start, $end) = split '-', $range;
+        next unless $start and $end;
+        for my $i (int($start) .. int($end))
+        {
+            $sum  += $i if $i =~ /^([0-9]+)\g{-1}$/;
+            $sum2 += $i if $i =~ /^([0-9]+)\g{-1}+$/;
+        }
+    } ## end foreach my $range (split ','...)
+} ## end while (<>)
+
+print "$sum\t$sum2\n";
