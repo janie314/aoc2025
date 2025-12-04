7d631b7cbbd35231c912dacb57b84d0b84ecd7ff
12ball <pub@janie.page>
2025-12-03 18:23:37 -0600

    Day 3

---
+++ 03b.pl
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+no warnings 'experimental::re_strict';
+use re 'strict';
+use List::Util;
+use List::MoreUtils;
+
+my $total = 0;
+
+while (<>) {
+    chomp;
+    my @nums  = map { int($_) } (split '');
+    my $start = 0;
+    my $stop  = scalar @nums - 12;
+    foreach my $i (0 .. 11) {
+        my $digit = List::Util::max(@nums[ $start .. $stop ]);
+        my $index = $start + List::MoreUtils::first_index { int($_) eq $digit } (@nums[ $start .. $stop ]);
+        $start = $index + 1;
+        $stop++;
+        $total += $digit * 10**(11 - $i);
+    }
+}
+
+print "$total\n";
