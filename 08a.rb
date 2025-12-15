a3b23f28421fce27acaa9d264a3cbd6893d3a73b
12ball <pub@janie.page>
2025-12-14 21:20:41 -0600

    1765768841

--- 08a.rb
+++ 08a.rb
@@ -20,29 +20,34 @@ end
 
 circuits = []
 count = 0
+
 pairs.each do |v, w|
   i = circuits.find_index { |circuit| circuit.any? v }
   j = circuits.find_index { |circuit| circuit.any? w }
-  puts "i #{i} j #{j}"
 
   count += if i.nil?
              if j.nil?
+               print 'a'
                circuits.append([v, w])
              else
+               print 'b'
                circuits[j].append(v)
              end
              1
            elsif j.nil?
+             print 'c'
              circuits[i].append(w)
              1
            elsif i == j
+             print 'd'
              0
            else
+             print 'e'
              circuits.append(circuits[i].concat(circuits[j]))
              circuits = circuits.reject.with_index { |_, k| k == i || k == j }
              1
            end
+  puts "c #{count}"
+  puts(circuits.map { |arr| arr.uniq.length }.sort.reverse.join('_'))
   break if count == 999
 end
-
-puts(circuits.map { |arr| arr.uniq.length }.join('-'))
