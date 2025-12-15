38e63925f90396a0963211b0d3253536025d5082
12ball <pub@janie.page>
2025-12-14 19:23:13 -0600

    8a

--- 08a.rb
+++ 08a.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 def dist(v, w)
   v.zip(w).map { |a, b| (a - b)**2 }.sum
 end
@@ -18,29 +20,28 @@ end
 
 circuits = []
 count = 0
-
 pairs.each do |v, w|
   i = circuits.find_index { |circuit| circuit.any? v }
   j = circuits.find_index { |circuit| circuit.any? w }
 
-  count += 1
-
-  if i.nil?
-    if j.nil?
-      circuits.append([v, w])
-    else
-      circuits[j].append(v)
-    end
-  elsif j.nil?
-    circuits[i].append(w)
-  elsif i != j
-    circuits.append(circuits[i].concat(circuits[j]))
-    circuits = circuits.reject.with_index { |_, k| k == i || k == j }
-  end
-
-  puts(circuits.map { |arr| arr.uniq.length }.sort.reverse.join('_'))
-
-  break if count == 1000
+  count += if i.nil?
+             if j.nil?
+               circuits.append([v, w])
+             else
+               circuits[j].append(v)
+             end
+             1
+           elsif j.nil?
+             circuits[i].append(w)
+             1
+           elsif i == j
+             0
+           else
+             circuits.append(circuits[i].concat(circuits[j]))
+             circuits = circuits.reject.with_index { |_, k| k == i || k == j }
+             1
+           end
+  break if count == 999
 end
 
 puts(circuits.map { |arr| arr.uniq.length }.sort.reverse.take(3).reduce { |a, b| a * b })
