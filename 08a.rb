abf24e71bd1a36c846a73c31606a7d0610092df7
12ball <pub@janie.page>
2025-12-15 10:55:17 -0600

    8a

--- 08a.rb
+++ 08a.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-require 'pry-byebug'
-
 def dist(v, w)
   v.zip(w).map { |a, b| (a - b)**2 }.sum
 end
 
-coords = File.read('testinput8').split("\n").map do |line|
+coords = File.read('input8').split("\n").map do |line|
   line.split(',').map(&:to_i)
 end
 
@@ -25,28 +23,24 @@ pairs.each do |v, w|
   i = circuits.find_index { |circuit| circuit.any? v }
   j = circuits.find_index { |circuit| circuit.any? w }
 
-  count += if i.nil?
-             if j.nil?
-               print "i,j nil\t\t\t"
-               circuits.append([v, w])
-             else
-               print "i nil, j not nil\t\t\t"
-               circuits[j].append(v)
-             end
-             1
-           elsif j.nil?
-             print "i not nil, j nil\t\t\t"
-             circuits[i].append(w)
-             1
-           elsif i == j
-             print "i == j == not nil\t\t\t"
-             0
-           else
-             print "i != j == not nil\t\t\t"
-             circuits.append(circuits[i].concat(circuits[j]))
-             circuits = circuits.reject.with_index { |_, k| k == i || k == j }
-             1
-           end
+  count += 1
+
+  if i.nil?
+    if j.nil?
+      circuits.append([v, w])
+    else
+      circuits[j].append(v)
+    end
+  elsif j.nil?
+    circuits[i].append(w)
+  elsif i != j
+    circuits.append(circuits[i].concat(circuits[j]))
+    circuits = circuits.reject.with_index { |_, k| k == i || k == j }
+  end
+
   puts(circuits.map { |arr| arr.uniq.length }.sort.reverse.join('_'))
-  break if count == 9
+
+  break if count == 1000
 end
+
+puts(circuits.map { |arr| arr.uniq.length }.sort.reverse.take(3).reduce { |a, b| a * b })
