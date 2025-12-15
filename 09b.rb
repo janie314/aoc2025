510fb6b6634c88701e7958cc48c36eedcaaccd27
12ball <pub@janie.page>
2025-12-15 15:25:45 -0600

    1765833945

--- 09b.rb
+++ 09b.rb
@@ -3,38 +3,39 @@
 require 'pry-byebug'
 require 'json'
 
-red_coords = File.read('testinput9').split("\n").map { |t| t.split(',').map(&:to_i) }
+def main
+  red_coords = File.read('input9').split("\n").map { |t| t.split(',').map(&:to_i) }
 
-green_coords = red_coords.map.with_index do |v, i|
-  w = red_coords[(i + 1) % red_coords.length]
-  if v[0] == w[0]
-    (([v[1], w[1]].min + 1)..([v[1], w[1]].max - 1)).map do |i|
-      [v[0], i]
+  cols = red_coords.map { |_, y| y }.max
+  grid = (0..red_coords.map { |x, _| x }.max).map { |_| Array.new(cols + 1) }
+  print_grid(grid)
+
+  red_coords.each_with_index do |v, i|
+    a, b = v
+    grid[a][b] = 'R'
+    w = red_coords[(i + 1) % red_coords.length]
+    if v[0] == w[0]
+      (([v[1], w[1]].min + 1)..([v[1], w[1]].max - 1)).map do |i|
+        grid[v[0]][i] = 'G'
+      end
+    elsif v[1] == w[1]
+      (([v[0], w[0]].min + 1)..([v[0], w[0]].max - 1)).map do |i|
+        grid[i][v[1]] = 'G'
+      end
     end
-  elsif v[1] == w[1]
-    (([v[0], w[0]].min + 1)..([v[0], w[0]].max - 1)).map do |i|
-      [i, v[1]]
-    end
   end
-end.flatten(1)
 
-puts "red #{red_coords.to_json}"
-puts "green #{green_coords.to_json}"
-
-res = Enumerator.product(red_coords, red_coords).map do |v, w|
-  a,  b  = v
-  c,  d  = w
-  if Enumerator.product([a, c].min..[a, c].max, [b, d].min..[b, d].max).all? do |u|
-    red_coords.any?(u) || green_coords.any?(u)
-  end
-    puts "noreject #{v.to_json} #{w.to_json}"
-    ((a - c + 1) * (b - d + 1)).abs
-  else
-    puts "reject #{v.to_json}#{w.to_json}"
-    0
-  end
+  print_grid(grid)
 end
 
-binding.pry
+def print_grid(grid)
+  grid.each do |row|
+    row.each do |entry|
+      print entry || '.'
+    end
+    puts
+  end
+  puts
+end
 
-puts res
+main
