de8a3be29f0c90ea53d94c698460bc332c4ca5bd
12ball <pub@janie.page>
2025-12-17 11:24:59 -0600

    1765992299

--- 09b.rb
+++ 09b.rb
@@ -9,41 +9,37 @@ end
 
 class Day9b
   def initialize
-    @red_coords = File.read('input9').split("\n").map do |t|
+    @red_coords = File.read('testinput9').split("\n").map do |t|
       x, y = t.split(',').map(&:to_i)
       { x: x, y: y }
     end
     @rows = @red_coords.map { |v| v[:x] }.max
     @cols = @red_coords.map { |v| v[:y] }.max
-    @horiz_sides = @red_coords.map.with_index do |v, i|
+    @vert_sides = @red_coords.map.with_index do |v, i|
       w = @red_coords[(i + 1) % @red_coords.length]
       if v[:x] == w[:x]
         a, b = [v[:y], w[:y]].minmax
-        { x: v[:x], y_range: (a..b) }
+        { x: v[:x], y_range_start: a, y_range_end: b }
       end
     end.compact
-    @vert_sides = @red_coords.map.with_index do |v, i|
+    @horiz_sides = @red_coords.map.with_index do |v, i|
       w = @red_coords[(i + 1) % @red_coords.length]
       if v[:y] == w[:y]
         a, b = [v[:x], w[:x]].minmax
-        { y: v[:y], x_range: (a..b) }
+        { y: v[:y], x_range_start: a, x_range_end: b }
       end
     end.compact
   end
 
   def inside?(v)
-    @horiz_sides.any? { |side| side[:x] <= v[:x] && side[:y_range].any?(v[:y]) } &&
-      @horiz_sides.any? { |side| side[:x] >= v[:x] && side[:y_range].any?(v[:y]) } &&
-      @vert_sides.any? { |side| side[:y] <= v[:y] && side[:x_range].any?(v[:x]) } &&
-      @vert_sides.any? { |side| side[:y] >= v[:y] && side[:x_range].any?(v[:x]) }
+    @vert_sides.any? { |side| side[:x] <= v[:x] && side[:y_range_start] <= v[:y] && v[:y] <= side[:y_range_end] } &&
+      @vert_sides.any? { |side| side[:x] >= v[:x] && side[:y_range_start] <= v[:y] && v[:y] <= side[:y_range_end] } &&
+      @horiz_sides.any? { |side| side[:y] <= v[:y] && side[:x_range_start] <= v[:x] && v[:x] <= side[:x_range_end] } &&
+      @horiz_sides.any? { |side| side[:y] >= v[:y] && side[:x_range_start] <= v[:x] && v[:x] <= side[:x_range_end] }
   end
 
   def answer
-    puts "r #{@red_coords.length}"
-    i = 0
     Enumerator.product(@red_coords, @red_coords).map do |v, w|
-      i += 1
-      puts i
       if [v, w, { x: v[:x], y: w[:y] }, { x: w[:x], y: v[:y] }].all? do |u|
         inside?(u)
       end
